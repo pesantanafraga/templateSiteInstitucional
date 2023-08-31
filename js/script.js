8f7b8df095c03d4d886b95d366eed475f052970c
pesantanafraga <pesantanafraga@gmail.com>
2023-08-31 15:00:55 -0300

    Corrigindo erros de responsividade e atualizando informações e layout do template

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
-//Botão de volta para cima
+//Menu
 
-let mybutton = document.getElementById("up-bottom");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }function topFunction() {
-    document.documentElement.scrollTop = 0;
-  }
-
-  //Menu Hamburguer
-
-  function menuHamburguer() {
-    var x = document.getElementById("menu-itens");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      x.className += " responsive";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  function menuHamburguer2() {
-    var x = document.getElementById("meuMenu");
+function menuResponsivo() {
+    var x = document.getElementById("menu-nav");
     if (x.className === "menu") {
       x.className += " responsive";
     } else {
       x.className = "menu";
     }
   }
+
+  //Scroll para cima
+
+let mybutton = document.getElementById("scroll-up");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+} function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
